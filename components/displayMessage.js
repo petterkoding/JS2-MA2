1c9ff7072a57372c5b8c533cb34472a876fdd046
Petter Iversen <petter.iversen90@gmail.com>
2021-04-23 12:32:17 +0200

    first commit

---
+++ components/displayMessage.js
@@ -0,0 +1,4 @@
+export function displayMessage(messageType, message, targetElement) {
+  const element = document.querySelector(targetElement);
+  element.innerHTML = `<div class="message ${messageType}">${message}</div>`;
+}
